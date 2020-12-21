54a2413c89919a9c6c077109d06bb66f15bd8ed0
stha-sadin <71333461+stha-sadin@users.noreply.github.com>
2020-12-21 16:02:27 +0545

    Update SecondActivity.java

--- MAD LAB 2/SecondActivity.java
+++ MAD LAB 2/SecondActivity.java
@@ -20,15 +20,15 @@ setContentView(R.layout.activity_second);
         t2= (TextView) findViewById(R.id.textView2);
         t3= (TextView) findViewById(R.id.textView3);
 
-        //Getting the Intent
+       
         Intent i = getIntent();
 
-        //Getting the Values from First Activity using the Intent received
+        
 name=i.getStringExtra("name_key");
 reg=i.getStringExtra("reg_key");
 dept=i.getStringExtra("dept_key");
 
-        //Setting the Values to Intent
+       
 t1.setText(name);
 t2.setText(reg);
 t3.setText(dept);
