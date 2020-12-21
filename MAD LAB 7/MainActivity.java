faefcb215929aca38d1fb345472d58330b6adbe8
stha-sadin <71333461+stha-sadin@users.noreply.github.com>
2020-12-21 15:58:27 +0545

    Update MainActivity.java

--- MAD LAB 7/MainActivity.java
+++ MAD LAB 7/MainActivity.java
@@ -16,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
     private static final int REQUEST_CODE_PERMISSION = 2;
     String mPermission = Manifest.permission.ACCESS_FINE_LOCATION;
 
-    // GPSTracker class
+    
     GPSTracker gps;
 
     @Override
@@ -31,35 +31,29 @@ public class MainActivity extends AppCompatActivity {
                 ActivityCompat.requestPermissions(this, new String[]{mPermission},
                         REQUEST_CODE_PERMISSION);
 
-                // If any permission above not allowed by user, this condition wil execute every time, else your else part will work
+              
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
 
         btnShowLocation = (Button) findViewById(R.id.button);
-
-        // show location button click event
         btnShowLocation.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View arg0) {
-                // create class object
+          
                 gps = new GPSTracker(MainActivity.this);
 
-                // check if GPS enabled
+              
                 if(gps.canGetLocation()){
-                    // if(gps.getLocation()!=null){
+                    
                     double latitude = gps.getLatitude();
                     double longitude = gps.getLongitude();
 
-                    // \n is for new line
                     Toast.makeText(getApplicationContext(), "Your Location is - \nLat: "
                             + latitude + "\nLong: " + longitude, Toast.LENGTH_LONG).show();
                 }else{
-                    // can't get location
-                    // GPS or Network is not enabled
-                    // Ask user to enable GPS/network in settings
                     gps.showSettingsAlert();
                 }
 
